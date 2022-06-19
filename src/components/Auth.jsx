2f898cb76a91a76982952250f7edb43623baff95
shanjidasultana <shanjidasultana2000@gmail.com>
2022-06-19 13:16:47 +0600

    updateing

--- src/components/Auth.jsx
+++ src/components/Auth.jsx
@@ -33,7 +33,7 @@ const Auth = () => {
 
         const { username, password, phoneNumber, avatarURL } = form;
 
-        const URL = 'http://localhost:5000/auth';
+        const URL = 'https://owl-app-server.herokuapp.com/auth';
       //  const URL = 'https://medical-pager.herokuapp.com/auth';
 
         const { data: { token, userId, hashedPassword, fullName } } = await axios.post(`${URL}/${isSignup ? 'signup' : 'login'}`, {
